11bb5104780fa13c891cd70530dc971598bd022b
Andre Holzner <holzner@andres-mbp-2.fritz.box>
2015-02-17 22:07:08 +0100

    added more verbosity in option description and error messages

--- wsPlot1D.py
+++ wsPlot1D.py
@@ -36,17 +36,16 @@ wsutils.addCommonOptions(parser,
 parser.add_option("--range",
                   dest="rangeSpec",
                   default = None,
-                  type="str",
-                  help="specify the plotting range",
-                  metavar="min,max")
+                  help="specify the range (min,max) for the variable on the x axis",
+                  metavar="min,max"
+                  )
 
 parser.add_option("-o",
                   dest="outputFile",
                   default = None,
-                  type="str",
-                  help="output file to save the plot to",
-                  metavar="FNAME")
-
+                  help="output file to save the ROOT canvas to (e.g. a png file or a C file etc.). WARNING: overwrites existing files without asking for confirmation",
+                  metavar="outfile"
+                  )
 
 (options, ARGV) = parser.parse_args()
 
@@ -58,6 +57,25 @@ wsutils.checkCommonOptions(options)
 if len(ARGV) < 3:
     print >> sys.stderr,"expected at least three positional arguments"
     sys.exit(1)
+
+#----------------------------------------
+
+if options.rangeSpec != None:
+
+    tmp = options.rangeSpec.split(',')
+    if len(tmp) != 2:
+        print >> sys.stderr,"malformed range specification " + options.rangeSpec
+        sys.exit(1)
+        
+    try:
+        options.rangeSpec = [ float(x) for x in tmp ]
+    except ValueError, ex:
+        print >> sys.stderr,"Error when parsing the range specification:",ex
+        sys.exit(1)
+
+    if rangeMin > rangeMax:
+        print >> sys.stderr,"range minimum is above range maximum"
+        sys.exit(1)
 
 #----------------------------------------
 
@@ -91,11 +109,8 @@ varname = ARGV.pop(0)
 var = wsutils.getObj(workspace, varname)
 
 if options.rangeSpec != None:
-    rangeMin, rangeMax = [ float(x) for x in options.rangeSpec.split(',') ]
-
-    assert rangeMin <= rangeMax
-
-    var.setRange(rangeMin, rangeMax)
+    var.setMin(options.rangeSpec[0])
+    var.setMax(options.rangeSpec[1])
 
 frame = var.frame()
 
